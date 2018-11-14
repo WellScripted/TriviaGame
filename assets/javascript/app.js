02da8326d17344261163443f65a6cd5464f65389
WellScripted <whigareda@theblackpilledpage.com>
2018-11-13 20:13:42 -0800

    Started Bootstrap, pseudocoding for app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+//Pseudocode for app.js
+
+//Questions:
+
+//Who was the 1st President of the United States of America?
+//Choices: Abe Lincoln, Jefferson Davis, Benjamin Franklin, George Washington
+//Answer: George Washington
+//Image of George Washington
+
+//What is our Nation's founding document?
+//Choices: Magna Carta, Emancipation Proclaimation, Articles of Confederation, Declaration of Independence
+//Answer: Declaration of Independence
+//Image of Declaration of Independence
+
+//What is the oldest State in the Union?
+//Choices: New York, Pennsylvania, Delaware, Texas
+//Answer: Delaware
+//Image of Delaware or it's State Flag
+
+//What year was the "Louisiana Purchase" made?
+//Choices: 1984, 1492, 1803, 1776
+//Answer: 1803
+//Image of the Louisiana Purchase
+
+//The "Reconstruction Era" began after the Revolutionary War ended in 1783
+//Choices: True or False
+//Answer: False
+//Alert: WRONG! The "Reconstruction Era" began after the end of the Civil War in 1865
+
+//Maryland was originally a colony founded for Catholics in the "New World"
+//Choices: True or False
+//Answer: True
+//Alert: Correct! It was founded by Lord Baltimore and other Catholics for religious freedom.
+
+//What year did the "War of 1812" begin?
+//Choices: 1811, 1810, 1812, 1815
+//Answer: 1812
+//Image of the War of 1812
