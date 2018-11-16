a5bb42255bbb19c8e94823be5fb2aaadd9ff735e
WellScripted <whigareda@theblackpilledpage.com>
2018-11-15 18:31:09 -0800

    Updated Game Logic & index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,35 @@
 //Pseudocode for app.js
 
+//Click Event for Start button:
+
+$('#start').on("click", function() {
+    game.start();
+});
+
+//Click Event for End Button:
+
+$(document).on("click", "#done", function() {
+    game.done();
+});
+
 //Questions:
+
+var questions = [{
+    question: "Who was the 1st President of the United States of America?",
+    choices: ["Abe Lincoln", "Jefferson Davis", "Benjamin Franklin", "George Washington"],
+    answer: "George Washington"
+    //Image of George Washington
+}, {
+    //Repeat same format for all 6 other questions
+},{
+    //3rd question
+},{
+    //4th question
+},{
+    //5th question
+},{
+    //6th question
+}]; 
 
 //Who was the 1st President of the United States of America?
 //Choices: Abe Lincoln, Jefferson Davis, Benjamin Franklin, George Washington
@@ -36,3 +65,41 @@
 //Choices: 1811, 1810, 1812, 1815
 //Answer: 1812
 //Image of the War of 1812
+
+var timer;
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function() {
+        game.counter--;
+        $("#counter").html(game.counter);
+        if (game.counter <= 0) {
+            game.done()
+        }
+    },
+
+    //Start method
+    start: function() {
+        timer = setInterval(game.countdown, 1000);
+        $("#sub-container").prepend("<h2> Time Remaining: <span id='counter'></span> Seconds: </h2>");
+        $("#start").remove();
+        //LOOP THROUGH QUESTIONS ARRAY HERE!
+    },
+    
+    //Done method
+    done: function() {
+        $.each("input[name='question-0']:checked"), function() {
+            if($(this).val() === questions[0].answer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        } //Create for each question in the array(name='question-1', question-2)
+        this.result() //this.result goes after all the each question
+    }, 
+    result: function() {
+
+    }
+   
+};
